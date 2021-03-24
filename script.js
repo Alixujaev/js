ca38cc0bb5bd6fc53c52dc5985427fb8cc566413
islom <islom@gmail.com>
2021-03-24 18:25:33 +0500

    main work

---
+++ script.js
@@ -0,0 +1,26 @@
+var myName = prompt('Ismingizni kiriting.');
+var yourAge = prompt('Yoshingizni kiriting.');
+var misol1 = prompt('Misolni yeching: 4 + 6 =');
+var misol2 = prompt('Misolni yeching: 12 - 7 =');
+var misol3 = prompt('Misolni yeching: 5 * 5 =');
+var misol4 = prompt('Misolni yeching: 8 / 2 =');
+var misol5 = prompt('Misolni yeching: 10 % 3 =');
+var javob = ('Javoblarni ko`rish uchun Console bo`limiga o`ting.');
+alert(javob);
+
+var sizningJavob = ('Sizning javob:');
+var togriJavob = ('To`g`ri javob:');
+
+var MIsol1 = (4+6);
+var MIsol2 = (12-7);
+var MIsol3 = (5*5);
+var MIsol4 = (8/2);
+var MIsol5 = (10%3);
+
+console.log('Sizning ismingiz '+ myName);
+console.log('Sizning yoshingiz '+ yourAge);
+console.log(sizningJavob  + '4 + 6 = ' + misol1 + ' ' + togriJavob + MIsol1);
+console.log(sizningJavob  + '12 - 7 = ' + misol2 + ' ' + togriJavob + MIsol2);
+console.log(sizningJavob  + '5 * 5 = ' + misol3 + ' ' + togriJavob + MIsol3);
+console.log(sizningJavob  + '8 / 2 = ' + misol4 + ' ' + togriJavob + MIsol4);
+console.log(sizningJavob  + '10 % 3 = ' + misol5 + ' ' + togriJavob + MIsol5);
